8a065642a54d23b17bc75bc7266c8bc9d8b1dd35
AlexzPurewoko <purwoko908@gmail.com>
2024-08-12 01:43:22 +0700

    add login controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class LoginController extends Controller
 {
-    //
+    public function __invoke(): View
+    {
+        return view("login");
+    }
 }
