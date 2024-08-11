d0e2499218dc98294d2d784b6e3f88c917982493
AlexzPurewoko <purwoko908@gmail.com>
2024-08-12 02:22:37 +0700

    use has

--- app/Gateways/UsersGateway.php
+++ app/Gateways/UsersGateway.php
@@ -3,6 +3,7 @@
 namespace App\Gateways;
 use App\Models\User;
 use Str;
+use Hash;
 
 class UsersGateway {
     public function newUser(string $email, string $password): void
@@ -11,6 +12,10 @@ class UsersGateway {
         preg_match('/^[^@]+/', $email, $prefixEmail);
 
         $username = $prefixEmail[0] ?? "user-". Str::random(8);
-        User::create(compact('username', 'email', 'password'));
+        User::create([
+            'username' => $username,
+            'email' => $email,
+            'password' => Hash::make($password)
+        ]);
     }
 }
